da7cc044daadae606b46f8926f29e1420f5b2318
Allisson Mateus <allissonmateus89@gmail.com>
2020-02-07 23:07:51 -0300

    Versão Final

--- src/Conjunto.js
+++ src/Conjunto.js
@@ -1,15 +1,23 @@
 class Cojunto{
    
-    constructor({nome,elements = []}){
+    constructor(conjunto){
+        const [nome,elementos] = conjunto.split('=')
         this.nome = nome 
-        this.elements = elements.replace('{','').replace('}','').split(',')
+        this.elements = elementos.replace('{','').replace('}','').split(',')
     }
 
     inserir(elemento){
-        if(this.elements.some(item=>item == elemento))
-            return `Elemento '${elemento}' já foi inserido`
+        const existe = this.elements.some(item=>{
+           return (typeof item == 'string' ? item : item[0]) == (typeof elemento == 'string' ? elemento : elemento[0])
+        })
+       
+        const aux = typeof elemento == 'string' ? elemento : `{${elemento[0]}}`
+
+        if(existe)
+            return `Elemento '${aux}' já foi inserido`
+
         this.elements.push(elemento)
-        return `Elemento '${elemento}' inserido com sucesso`
+        return `Elemento '${aux}' inserido com sucesso`
     }
 
     tamanho(){
@@ -17,7 +25,11 @@ class Cojunto{
     }
 
     elementos(){
-        return  `${this.nome} = {${this.elements.join()}}`
+        let aux = '';
+        this.elements.forEach((element,index)=>{
+            aux = `${aux + (index > 0 ? "," : "")}${(typeof element == "string" ? element : "{"+element[0]+"}")}`
+        })
+        return `${this.nome}={${aux}}`
     }
 
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const Conjunto = require('./Conjunto')
 
-const conjunto = new Conjunto({
-    nome : 'Teste',
-    elements : '{A,B,C}'
-})
+const conjunto = new Conjunto('Teste={A,B,C,{K}}')
 
 console.log(conjunto.tamanho())
 console.log(conjunto.elementos())
-console.log(conjunto.inserir('D'))
+console.log(conjunto.inserir('{D}'))
 console.log(conjunto.elementos())
+console.log(conjunto.inserir('X'))
+console.log(conjunto.elementos())
+console.log(conjunto.tamanho())
